59b18c434bd0203f36868cc06e8d07e406303690
stackotter <stackotter@stackotter.dev>
2021-10-22 20:34:01 +1000

    Fix prebuild command
    I forgot to add it to the list of commands

--- Sources/swift-bundler/Bundler.swift
+++ Sources/swift-bundler/Bundler.swift
@@ -2,5 +2,5 @@ import Foundation
 import ArgumentParser
 
 struct Bundler: ParsableCommand {
-  static let configuration = CommandConfiguration(subcommands: [Init.self, GenerateXcodeSupport.self, Build.self, Run.self, Prebuild.self, Bundle.self, RemoveFileHeaders.self])
+  static let configuration = CommandConfiguration(subcommands: [Init.self, GenerateXcodeSupport.self, Build.self, Run.self, Prebuild.self, Postbuild.self, Bundle.self, RemoveFileHeaders.self])
 }
