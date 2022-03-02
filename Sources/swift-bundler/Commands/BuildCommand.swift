43ae1f32ec27988ac5a1739be59049a4396f40ab
stackotter <stackotter@stackotter.dev>
2022-03-02 21:37:58 +1000

    Load configuration from Bundler.toml

--- Sources/swift-bundler/Commands/BuildCommand.swift
+++ Sources/swift-bundler/Commands/BuildCommand.swift
@@ -5,8 +5,12 @@ import TOMLKit
 struct BuildCommand: ParsableCommand {
   static var configuration = CommandConfiguration(commandName: "build")
   
+  @Argument(
+    help: "The name of the app to build")
+  var appName: String?
+  
   @Option(
-    name: [.customLong("directory"), .customShort("d")],
+    name: [.customShort("d"), .customLong("directory")],
     help: "The directory containing the package to be bundled",
     transform: URL.init(fileURLWithPath:))
   var packageDirectory = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
@@ -26,29 +30,35 @@ struct BuildCommand: ParsableCommand {
   var outputDirectory: URL?
 
   @Flag(
-    name: [.customShort("u"), .customLong("universal")],
+    name: .shortAndLong,
     help: "Build a universal application (arm and intel)")
   var universal = false
 
   func run() throws {
-    let configuration = AppConfiguration(
-      target: "HelloWorld",
-      version: "0.1.0",
-      category: "example",
-      bundleIdentifier: "dev.stackotter.test-app",
-      minMacOSVersion: "11.0",
-      plistEntries: [:])
+    let configuration = try ConfigurationUtil.loadConfiguration(fromDirectory: packageDirectory)
+    let appConfiguration: AppConfiguration
+    if let first = configuration.apps.first, configuration.apps.count == 1 {
+      appConfiguration = first.value
+    } else if let appName = appName {
+      guard let selected = configuration.apps[appName] else {
+        throw ConfigurationError.failedToGetSelectedApp(appName)
+      }
+      appConfiguration = selected
+    } else {
+      log.error("There are multiple apps defined. Please use the `appName` argument to specify which one to build.")
+      return
+    }
     
     let outputDirectory = outputDirectory ?? packageDirectory.appendingPathComponent(".build/bundler")
     let productsDirectory = packageDirectory.appendingPathComponent(".build/\(buildConfiguration)")
     
     let bundler = Bundler(.init(
-      appConfiguration: configuration,
+      appConfiguration: appConfiguration,
       buildConfiguration: buildConfiguration,
       packageDirectory: packageDirectory,
       productsDirectory: productsDirectory,
       outputDirectory: outputDirectory,
-      appName: "HelloWorldApp",
+      appName: appName ?? configuration.apps.first!.key,
       universal: universal))
     
     try bundler.prebuild()
