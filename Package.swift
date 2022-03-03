ee1354cfe9bf3d8e66e7d8f7713f5bde6651602e
stackotter <stackotter@stackotter.dev>
2022-03-03 22:12:31 +1000

    Add basic variable substitution to plist entries

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-log", from: "1.4.2"),
-    .package(url: "https://github.com/LebJe/TOMLKit.git", from: "0.5.0")
+    .package(url: "https://github.com/LebJe/TOMLKit.git", from: "0.5.0"),
+    .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.7.1")
   ],
   targets: [
     .target(
@@ -19,7 +20,8 @@ let package = Package(
       dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
         .product(name: "Logging", package: "swift-log"),
-        "TOMLKit",
-      ]),
+        .product(name: "Parsing", package: "swift-parsing"),
+        "TOMLKit"
+      ])
   ]
 )
