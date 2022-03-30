d0ccf7c6ffedbaa60d0e5db2286b8984a7c37bcf
stackotter <stackotter@stackotter.dev>
2022-03-30 17:33:40 +1000

    Add error descriptions for MetalCompiler

---
+++ Sources/swift-bundler/Bundler/MetalCompilerError.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+/// An error returned by ``MetalCompiler``.
+enum MetalCompilerError: LocalizedError {
+  case failedToCreateTemporaryCompilationDirectory(URL, Error)
+  case failedToCompileShader(URL, ProcessError)
+  case failedToCreateMetalArchive(ProcessError)
+  case failedToCreateMetalLibrary(ProcessError)
+  case failedToDeleteShaderSource(URL, Error)
+  case failedToEnumerateShaders(directory: URL)
+  
+  var errorDescription: String? {
+    switch self {
+      case .failedToCreateTemporaryCompilationDirectory(let directory, _):
+        return "Failed to create a temporary directory for shader compilation at '\(directory.path)'"
+      case .failedToCompileShader(let file, let processError):
+        return "Failed to compile shader source file '\(file.path)': \(processError.localizedDescription)"
+      case .failedToCreateMetalArchive(let processError):
+        return "Failed to create a metal archive from compiled source files: \(processError.localizedDescription)"
+      case .failedToCreateMetalLibrary(let processError):
+        return "Failed to create a metal library from the metal archive: \(processError.localizedDescription)"
+      case .failedToDeleteShaderSource(let file, _):
+        return "Failed to delete the shader source file at '\(file.path)' after compilation"
+      case .failedToEnumerateShaders(let directory):
+        return "Failed to enumerate shaders in directory '\(directory.path)'"
+    }
+  }
+}
