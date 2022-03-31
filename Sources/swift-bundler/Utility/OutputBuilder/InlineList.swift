c786b4f25ce539426fa1d8daaccea668523c61e7
stackotter <stackotter@stackotter.dev>
2022-03-31 10:42:15 +1000

    Create resultBuilder for constructing command-line output

---
+++ Sources/swift-bundler/Utility/OutputBuilder/InlineList.swift
@@ -0,0 +1,15 @@
+/// A component that displays an array of strings as an inline list.
+struct InlineList: OutputComponent {
+  /// The elements to display as an inline list.
+  var elements: [String]
+  
+  var body: String {
+    "[" + elements.joined(separator: ", ") + "]"
+  }
+  
+  /// Creates a component that displays an array of strings as an inline list.
+  /// - Parameter elements: Elements of the list to display.
+  init(_ elements: [String]) {
+    self.elements = elements
+  }
+}
---
+++ Sources/swift-bundler/Utility/OutputBuilder/StringOutput.swift
@@ -0,0 +1,10 @@
+/// A simple output component that displays the string that it is given. Used to avoid an infinite loop that would otherwise occur in `String.body`.
+struct StringOutput: OutputComponent {
+  var body: String
+  
+  /// Creates an output component that displays a string.
+  /// - Parameter content: The string to display.
+  init(_ content: String) {
+    body = content
+  }
+}
