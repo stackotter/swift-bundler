3ced530b16f98f5c361bd0c0c8b559c36a0bef4f
stackotter <stackotter@stackotter.dev>
2022-03-31 17:53:34 +1000

    Add more logging to the install script and ensure Xcode CLTs are installed

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 # Exit on error
 set -e
 
+# Ensure that xcode command-line tools are installed
+echo "Ensuring that Xcode CLTs are installed"
+xcode-select --install 2>/dev/null || true
+
 # Build
+echo "Building swift-bundler"
 swift build -c release
 
 # Create directory with correct permissions
@@ -13,7 +18,7 @@ sudo mkdir -p -m755 /opt/swift-bundler
 sudo cp .build/release/swift-bundler /opt/swift-bundler
 
 # Add to PATH if it isn't in PATH already
-[[ ":$PATH:" != *":/opt/swift-bundler:"* ]] && printf "\nexport PATH=\"/opt/swift-bundler:\$PATH\"" >> ~/.zshenv && echo "Run \`source ~/.zshenv\` for new PATH to take effect"
+[[ ":$PATH:" != *":/opt/swift-bundler:"* ]] && printf "\nexport PATH=\"/opt/swift-bundler:\$PATH\"" >> ~/.zshenv && echo "\nRun \`source ~/.zshenv\` to finish installation"
 
 # Exit with the success code (the previous command makes the exit code a failure when /opt/swift-bundler is already in PATH)
 exit 0
