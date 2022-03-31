080db9b42c75aa88fee0bb7742d37424b700d08a
stackotter <stackotter@stackotter.dev>
2022-03-31 11:00:45 +1000

    Add pretty output and command suggestions to TemplatesListCommand

--- Sources/swift-bundler/Utility/OutputBuilder/OutputDictionaryBuilder.swift
+++ Sources/swift-bundler/Utility/OutputBuilder/OutputDictionaryBuilder.swift
@@ -3,4 +3,24 @@
   static func buildBlock(_ components: OutputDictionary.Entry...) -> [OutputDictionary.Entry] {
     components
   }
+  
+  static func buildBlock(_ components: [OutputDictionary.Entry]...) -> [OutputDictionary.Entry] {
+    components.flatMap { $0 }
+  }
+  
+  static func buildArray(_ components: [[OutputDictionary.Entry]]) -> [OutputDictionary.Entry] {
+    components.flatMap { $0 }
+  }
+  
+  static func buildOptional(_ component: [OutputDictionary.Entry]?) -> [OutputDictionary.Entry] {
+    component ?? []
+  }
+  
+  static func buildEither(first component: [OutputDictionary.Entry]) -> [OutputDictionary.Entry] {
+    component
+  }
+  
+  static func buildEither(second component: [OutputDictionary.Entry]) -> [OutputDictionary.Entry] {
+    component
+  }
 }
