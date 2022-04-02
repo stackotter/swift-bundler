dd87ac6f00044bad44f8bdd4396a5a4d5597d8b2
stackotter <stackotter@stackotter.dev>
2022-04-03 08:24:52 +1000

    Bump version to v2.0.1

--- Sources/swift-bundler/SwiftBundler.swift
+++ Sources/swift-bundler/SwiftBundler.swift
@@ -7,7 +7,7 @@ struct SwiftBundler: ParsableCommand {
   static let configuration = CommandConfiguration(
     commandName: "swift-bundler",
     abstract: "A tool for creating macOS apps from Swift packages.",
-    version: "v2.0.0",
+    version: "v2.0.1",
     shouldDisplay: true,
     subcommands: [
       BundleCommand.self,
