991bf0374254edb797cfaa50c330dfbc60e4eb1d
stackotter <stackotter@stackotter.dev>
2022-04-02 10:03:41 +1000

    Update CLI version to v2.0.0

--- Sources/swift-bundler/SwiftBundler.swift
+++ Sources/swift-bundler/SwiftBundler.swift
@@ -7,7 +7,7 @@ struct SwiftBundler: ParsableCommand {
   static let configuration = CommandConfiguration(
     commandName: "swift-bundler",
     abstract: "A tool for creating macOS apps from Swift packages.",
-    version: "v2.0.0-rc.1",
+    version: "v2.0.0",
     shouldDisplay: true,
     subcommands: [
       BundleCommand.self,
