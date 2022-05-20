21720ca70ae66dd2a8915b0887e8a8b834c14152
stackotter <stackotter@stackotter.dev>
2022-05-20 19:03:31 +1000

    Keep on-disk structure of target source files

--- Sources/swift-bundler/Commands/ConvertCommand.swift
+++ Sources/swift-bundler/Commands/ConvertCommand.swift
@@ -37,11 +37,56 @@ struct ConvertCommand: Command {
     }
 
     let project = try XcodeProj(pathString: xcodeProjectFile.path)
-    let sourceRoot = xcodeProjectFile.deletingLastPathComponent().path
+    let sourceRoot = xcodeProjectFile.deletingLastPathComponent()
+    
+    struct XcodeFile {
+      var path: String
+      var sourceTree: PBXSourceTree
+      var parent: PBXFileElement?
+
+      func absolutePath(sourceRoot: URL) throws -> URL {
+        switch sourceTree {
+          case .absolute:
+            return URL(fileURLWithPath: path)
+          default:
+            return sourceRoot.appendingPathComponent(try relativePath())
+        }
+      }
+
+      func relativePath() throws -> String {
+        switch sourceTree {
+          case .absolute:
+            return URL(fileURLWithPath: path).lastPathComponent
+          case .sourceRoot:
+            return path
+          case .group:
+            guard let parent = parent, let sourceTree = parent.sourceTree else {
+              return path
+            }
+
+            let parentGroup = XcodeFile(
+              path: parent.path ?? "",
+              sourceTree: sourceTree,
+              parent: parent.parent
+            )
+
+            let parentPath = try parentGroup.relativePath()
+            if path != "" && parentPath != "" {
+              return parentPath + "/" + path
+            } else if parentPath != "" {
+              return parentPath
+            } else {
+              return path
+            }
+          default:
+            fatalError("Unsupported path type: \(sourceTree)")
+        }
+      }
+    }
 
     struct Target {
       var name: String
-      var resourcePaths: [String]
+      var resources: [XcodeFile]
     }
 
     var targets: [Target] = []
@@ -51,29 +96,46 @@ struct ConvertCommand: Command {
       let name = target.name
 
       guard target.productType == .application else {
-        log.warning("Non-executable targets not yet supported. Skipping '\(name)'.")
+        log.warning("Non-executable targets not yet supported. Skipping '\(name)'")
         continue
       }
 
       log.info("Converting target '\(name)'")
 
       // Enumerate the target's files
-      var filesToCopy: [String] = []
+      var filesToCopy: [XcodeFile] = []
 
-      let sourceFiles = try target.sourceFiles().compactMap { file in
-        return try file.fullPath(sourceRoot: sourceRoot)
+      let sourceFiles = try target.sourceFiles().compactMap { file -> XcodeFile? in
+        guard 
+          let path = file.path,
+          let sourceTree = file.sourceTree
+        else {
+          log.warning("Skipping invalid source file '\(file.name ?? "Unknown name")'")
+          return nil
+        }
+
+        return XcodeFile(path: path, sourceTree: sourceTree, parent: file.parent)
       }
 
       filesToCopy += sourceFiles
 
-      var resourcePaths: [String] = []
+      var resources: [XcodeFile] = []
       if let resourceFiles = try? target.resourcesBuildPhase()?.files {
-        let fullPaths = try resourceFiles.compactMap { file in
-          return try file.file?.fullPath(sourceRoot: sourceRoot)
+        let files = resourceFiles.compactMap { file -> XcodeFile? in
+          guard
+            let file = file.file,
+            let path = file.path,
+            let sourceTree = file.sourceTree
+          else {
+            log.warning("Skipping invalid resource file '\(file.file?.name ?? "Unknown name")'")
+            return nil
+          }
+
+          return XcodeFile(path: path, sourceTree: sourceTree, parent: file.parent)
         }
 
-        filesToCopy += fullPaths
-        resourcePaths = resourceFiles.compactMap(\.file?.path)
+        filesToCopy += files
+        resources = files
       }
 
       // Create output directory
@@ -81,23 +143,39 @@ struct ConvertCommand: Command {
       try FileManager.default.createDirectory(at: targetDirectory)
 
       // Copy target's files to output directory
-      for file in filesToCopy.map(URL.init(fileURLWithPath:)) {
-        try FileManager.default.copyItem(at: file, to: targetDirectory.appendingPathComponent(file.lastPathComponent))
+      for file in filesToCopy {
+        // Get source and destination
+        let absolutePath = try file.absolutePath(sourceRoot: sourceRoot)
+        var relativePath = try file.relativePath()
+        if relativePath.hasPrefix(name) {
+          // Remove the redundant initial folder and the following '/'
+          relativePath.removeFirst(name.count + 1)
+        }
+        
+        let destination = targetDirectory.appendingPathComponent(relativePath)
+
+        // Create parent directory if required
+        let directory = destination.deletingLastPathComponent()
+        if !FileManager.default.fileExists(atPath: directory.path) {
+          try FileManager.default.createDirectory(at: directory)
+        }
+
+        try FileManager.default.copyItem(at: absolutePath, to: destination)
       }
 
-      targets.append(Target(name: name, resourcePaths: resourcePaths))
+      targets.append(Target(name: name, resources: resources))
     }
 
     let targetsString = targets.map { target in
-      let resourcesString = target.resourcePaths.map { path in
-        return "                .process(\"\(path)\")"
+      let resourcesString = target.resources.map { file in
+        return "                .process(\"\(file.path)\")"
       }.joined(separator: ",\n")
       return """
         .executableTarget(
             name: "\(target.name)",
             dependencies: [],
             resources: [
-                \(resourcesString)
+\(resourcesString)
             ]
         )
 """
@@ -113,7 +191,7 @@ let package = Package(
     platforms: [.macOS(.v11), .iOS(.v14)],
     dependencies: [],
     targets: [
-        \(targetsString)
+\(targetsString)
     ]
 )
 """
