a4adb8d9558576aaf8e2114fb1f1200ce71e355e
stackotter <stackotter@stackotter.dev>
2023-09-16 12:29:40 +1000

    Update package to use stackotter fork of swift-argument-parser again (after merging PR)

--- Package.swift
+++ Package.swift
@@ -9,18 +9,20 @@ let package = Package(
     .executable(name: "swift-bundler", targets: ["swift-bundler"])
   ],
   dependencies: [
-    .package(url: "https://github.com/Wabi-Studios/swift-arg-parser.git", from: "1.2.4"),
+    .package(url: "https://github.com/stackotter/swift-arg-parser", from: "1.2.3"),
     .package(url: "https://github.com/apple/swift-log", from: "1.4.2"),
-    .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.7.1"),
+    .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.7.1"),
     .package(url: "https://github.com/LebJe/TOMLKit", branch: "main"),
     .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
     .package(url: "https://github.com/mxcl/Version.git", from: "2.0.0"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     .package(url: "https://github.com/stackotter/XcodeGen", exact: "2.35.1"),
     .package(
-      url: "https://github.com/apple/swift-syntax", exact: "0.50800.0-SNAPSHOT-2022-12-29-a"),
+      url: "https://github.com/apple/swift-syntax", exact: "0.50800.0-SNAPSHOT-2022-12-29-a"
+    ),
     .package(
-      url: "https://github.com/apple/swift-format", exact: "0.50800.0-SNAPSHOT-2022-12-29-a"),
+      url: "https://github.com/apple/swift-format", exact: "0.50800.0-SNAPSHOT-2022-12-29-a"
+    ),
     .package(url: "https://github.com/pointfreeco/swift-overture", from: "0.5.0"),
   ],
   targets: [
