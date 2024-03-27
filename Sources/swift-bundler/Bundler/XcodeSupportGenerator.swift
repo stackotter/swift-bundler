13544b24562a38e220ee47a6ad084a3716dc57a3
stackotter <stackotter@stackotter.dev>
2024-03-27 11:38:45 +1000

    Escape spaces in paths when generating xcschemes (#49)

--- Sources/swift-bundler/Bundler/XcodeSupportGenerator.swift
+++ Sources/swift-bundler/Bundler/XcodeSupportGenerator.swift
@@ -27,8 +27,11 @@ enum XcodeSupportGenerator {
   /// Gets the location to create Xcode schemes for a given package.
   /// - Parameter packageDirectory: The root directory of the package.
   /// - Returns: The package's schemes directory. If an error occurs, a failure is returned.
-  private static func getSchemesDirectory(in packageDirectory: URL) -> Result<URL, XcodeSupportGeneratorError> {
-    let schemesDirectory = packageDirectory.appendingPathComponent(".swiftpm/xcode/xcshareddata/xcschemes")
+  private static func getSchemesDirectory(in packageDirectory: URL) -> Result<
+    URL, XcodeSupportGeneratorError
+  > {
+    let schemesDirectory = packageDirectory.appendingPathComponent(
+      ".swiftpm/xcode/xcshareddata/xcschemes")
     do {
       try FileManager.default.createDirectory(at: schemesDirectory)
     } catch {
@@ -100,34 +103,57 @@ enum XcodeSupportGenerator {
 
     // The escaped strings required to fill in the template
     let escapedOutputPath = outputDirectory.path
+      .replacingOccurrences(of: " ", with: "\\ ")
       .replacingOccurrences(of: "\"", with: "\\\"")
     let escapedOutputBundlePath = outputAppBundle.path
-      .replacingOccurrences(of: "\"", with: "")
+      .replacingOccurrences(of: " ", with: "\\ ")
+      .replacingOccurrences(of: "\"", with: "\\\"")
     let packagePath = "${WORKSPACE_PATH}/../../../"
 
     // Commands to put in the scheme
-    let fixPath = "export PATH=`zsh --login -c '[ -f /etc/zshrc ] && . /etc/zshrc; [ -f ~/.zshrc ] && . ~/.zshrc; echo $PATH'`"
+    let fixPath =
+      "export PATH=`zsh --login -c '[ -f /etc/zshrc ] && . /etc/zshrc; [ -f ~/.zshrc ] && . ~/.zshrc; echo $PATH'`"
     let command = "swift-bundler bundle"
-    let arguments = "\(app) -d \(packagePath) --products-directory ${BUILT_PRODUCTS_DIR} -o '\(escapedOutputPath)' --skip-build --built-with-xcode"
+    let arguments =
+      "\(app) -d \(packagePath) --products-directory ${BUILT_PRODUCTS_DIR} -o '\(escapedOutputPath)' --skip-build --built-with-xcode"
     let createBundle = "\(fixPath); \(command) \(arguments)"
       .replacingOccurrences(of: "&", with: "&amp;")
 
     // Create the scheme's contents from a massive template
-    return .success("""
-<?xml version="1.0" encoding="UTF-8"?>
-<Scheme
-   LastUpgradeVersion = "1300"
-   version = "1.7">
-   <BuildAction
-      parallelizeBuildables = "YES"
-      buildImplicitDependencies = "YES">
-      <PostActions>
-         <ExecutionAction
-            ActionType = "Xcode.IDEStandardExecutionActionsCore.ExecutionActionType.ShellScriptAction">
-            <ActionContent
-               title = "Bundle executable"
-               scriptText = "\(createBundle)">
-               <EnvironmentBuildable>
+    return .success(
+      """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <Scheme
+         LastUpgradeVersion = "1300"
+         version = "1.7">
+         <BuildAction
+            parallelizeBuildables = "YES"
+            buildImplicitDependencies = "YES">
+            <PostActions>
+               <ExecutionAction
+                  ActionType = "Xcode.IDEStandardExecutionActionsCore.ExecutionActionType.ShellScriptAction">
+                  <ActionContent
+                     title = "Bundle executable"
+                     scriptText = "\(createBundle)">
+                     <EnvironmentBuildable>
+                        <BuildableReference
+                           BuildableIdentifier = "primary"
+                           BlueprintIdentifier = "\(product)"
+                           BuildableName = "\(product)"
+                           BlueprintName = "\(product)"
+                           ReferencedContainer = "container:">
+                        </BuildableReference>
+                     </EnvironmentBuildable>
+                  </ActionContent>
+               </ExecutionAction>
+            </PostActions>
+            <BuildActionEntries>
+               <BuildActionEntry
+                  buildForTesting = "YES"
+                  buildForRunning = "YES"
+                  buildForProfiling = "YES"
+                  buildForArchiving = "NO"
+                  buildForAnalyzing = "YES">
                   <BuildableReference
                      BuildableIdentifier = "primary"
                      BlueprintIdentifier = "\(product)"
@@ -135,84 +161,67 @@ enum XcodeSupportGenerator {
                      BlueprintName = "\(product)"
                      ReferencedContainer = "container:">
                   </BuildableReference>
-               </EnvironmentBuildable>
-            </ActionContent>
-         </ExecutionAction>
-      </PostActions>
-      <BuildActionEntries>
-         <BuildActionEntry
-            buildForTesting = "YES"
-            buildForRunning = "YES"
-            buildForProfiling = "YES"
-            buildForArchiving = "NO"
-            buildForAnalyzing = "YES">
-            <BuildableReference
-               BuildableIdentifier = "primary"
-               BlueprintIdentifier = "\(product)"
-               BuildableName = "\(product)"
-               BlueprintName = "\(product)"
-               ReferencedContainer = "container:">
-            </BuildableReference>
-         </BuildActionEntry>
-      </BuildActionEntries>
-   </BuildAction>
-   <TestAction
-      buildConfiguration = "Debug"
-      selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
-      selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
-      shouldUseLaunchSchemeArgsEnv = "YES">
-      <Testables>
-      </Testables>
-   </TestAction>
-   <LaunchAction
-      buildConfiguration = "Debug"
-      selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
-      selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
-      launchStyle = "0"
-      useCustomWorkingDirectory = "NO"
-      ignoresPersistentStateOnLaunch = "NO"
-      debugDocumentVersioning = "YES"
-      debugServiceExtension = "internal"
-      allowLocationSimulation = "YES">
-      <PathRunnable
-         runnableDebuggingMode = "0"
-         FilePath = "\(escapedOutputBundlePath)">
-      </PathRunnable>
-      <MacroExpansion>
-         <BuildableReference
-            BuildableIdentifier = "primary"
-            BlueprintIdentifier = "\(product)"
-            BuildableName = "\(product)"
-            BlueprintName = "\(product)"
-            ReferencedContainer = "container:">
-         </BuildableReference>
-      </MacroExpansion>
-   </LaunchAction>
-   <ProfileAction
-      buildConfiguration = "Release"
-      shouldUseLaunchSchemeArgsEnv = "YES"
-      savedToolIdentifier = ""
-      useCustomWorkingDirectory = "NO"
-      debugDocumentVersioning = "YES">
-      <BuildableProductRunnable
-         runnableDebuggingMode = "0">
-         <BuildableReference
-            BuildableIdentifier = "primary"
-            BlueprintIdentifier = "\(product)"
-            BuildableName = "\(product)"
-            BlueprintName = "\(product)"
-            ReferencedContainer = "container:">
-         </BuildableReference>
-      </BuildableProductRunnable>
-   </ProfileAction>
-   <AnalyzeAction
-      buildConfiguration = "Debug">
-   </AnalyzeAction>
-   <ArchiveAction
-      buildConfiguration = "Release"
-      revealArchiveInOrganizer = "YES">
-   </ArchiveAction>
-</Scheme>
-""")
+               </BuildActionEntry>
+            </BuildActionEntries>
+         </BuildAction>
+         <TestAction
+            buildConfiguration = "Debug"
+            selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
+            selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
+            shouldUseLaunchSchemeArgsEnv = "YES">
+            <Testables>
+            </Testables>
+         </TestAction>
+         <LaunchAction
+            buildConfiguration = "Debug"
+            selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
+            selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
+            launchStyle = "0"
+            useCustomWorkingDirectory = "NO"
+            ignoresPersistentStateOnLaunch = "NO"
+            debugDocumentVersioning = "YES"
+            debugServiceExtension = "internal"
+            allowLocationSimulation = "YES">
+            <PathRunnable
+               runnableDebuggingMode = "0"
+               FilePath = "\(escapedOutputBundlePath)">
+            </PathRunnable>
+            <MacroExpansion>
+               <BuildableReference
+                  BuildableIdentifier = "primary"
+                  BlueprintIdentifier = "\(product)"
+                  BuildableName = "\(product)"
+                  BlueprintName = "\(product)"
+                  ReferencedContainer = "container:">
+               </BuildableReference>
+            </MacroExpansion>
+         </LaunchAction>
+         <ProfileAction
+            buildConfiguration = "Release"
+            shouldUseLaunchSchemeArgsEnv = "YES"
+            savedToolIdentifier = ""
+            useCustomWorkingDirectory = "NO"
+            debugDocumentVersioning = "YES">
+            <BuildableProductRunnable
+               runnableDebuggingMode = "0">
+               <BuildableReference
+                  BuildableIdentifier = "primary"
+                  BlueprintIdentifier = "\(product)"
+                  BuildableName = "\(product)"
+                  BlueprintName = "\(product)"
+                  ReferencedContainer = "container:">
+               </BuildableReference>
+            </BuildableProductRunnable>
+         </ProfileAction>
+         <AnalyzeAction
+            buildConfiguration = "Debug">
+         </AnalyzeAction>
+         <ArchiveAction
+            buildConfiguration = "Release"
+            revealArchiveInOrganizer = "YES">
+         </ArchiveAction>
+      </Scheme>
+      """
+    )
   }
 }
