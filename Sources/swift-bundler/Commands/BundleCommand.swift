32a7a2ef076ca61be79e437b2cd4b2a1be29595f
furby™ <devs@wabi.foundation>
2024-08-16 12:36:47 -0600

    Use provided CLI output directory or default for xcodebuild.
    Signed-off-by: furby™ <devs@wabi.foundation>

--- Sources/swift-bundler/Commands/BundleCommand.swift
+++ Sources/swift-bundler/Commands/BundleCommand.swift
@@ -190,6 +190,15 @@ struct BundleCommand: AsyncCommand {
       let universal = arguments.universal || arguments.architectures.count > 1
       let architectures = getArchitectures(platform: arguments.platform)
 
+      var forceUsingXcodeBuild = isUsingXcodeBuild
+      // For all apple platforms (not including macOS), we generate xcode
+      // support, because macOS cannot cross-compile for any of the other
+      // darwin platforms like it can with linux, and thus we need to use
+      // xcodebuild to build for these platforms (ex. visionOS, iOS, etc)
+      if forceUsingXcodeBuild || ![Platform.linux, Platform.macOS].contains(arguments.platform) {
+        forceUsingXcodeBuild = true
+      }
+
       let outputDirectory = Self.getOutputDirectory(
         arguments.outputDirectory,
         scratchDirectory: scratchDirectory
