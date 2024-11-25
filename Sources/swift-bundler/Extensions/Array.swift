53bfce51abae47951460475cde42fe2eba60c46b
stackotter <stackotter@stackotter.dev>
2024-11-25 11:17:17 +1000

    Fix GraphViz dependency and retroactive conformance warnings

--- Sources/swift-bundler/Extensions/Array.swift
+++ Sources/swift-bundler/Extensions/Array.swift
@@ -1,7 +1,11 @@
 import Foundation
 import StackOtterArgParser
 
-extension Array: ExpressibleByArgument where Element: ExpressibleByArgument {
+// Fully qualified protocol names used to silence retroactive conformance warning
+// while still supporting older Swift versions (from before the @retroactive
+// marker).
+extension Array: StackOtterArgParser.ExpressibleByArgument
+where Element: StackOtterArgParser.ExpressibleByArgument {
   public var defaultValueDescription: String {
     "[" + self.map(\.defaultValueDescription).joined(separator: ", ") + "]"
   }
