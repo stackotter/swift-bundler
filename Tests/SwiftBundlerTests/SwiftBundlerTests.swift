b375fe9dc346d9df05e6f39555d24935f6ed17ef
stackotter <stackotter@stackotter.dev>
2025-05-15 00:32:42 +1000

    Add basic integration tests for the CLI and run tests in all CI

--- Tests/SwiftBundlerTests/SwiftBundlerTests.swift
+++ Tests/SwiftBundlerTests/SwiftBundlerTests.swift
@@ -1,24 +1,49 @@
-import XCTest
+import Testing
+import Foundation
 
 @testable import SwiftBundler
 
-final class SwiftBundlerTests: XCTestCase {
-  func testCommandLineParsing() throws {
-    let commandLine = CommandLine.lenientParse(
-      "./path/to/my\\ command arg1 'arg2 with spaces' \"arg3 with spaces\" arg4\\ with\\ spaces"
-    )
+@Test func testCommandLineParsing() throws {
+  let commandLine = CommandLine.lenientParse(
+    "./path/to/my\\ command arg1 'arg2 with spaces' \"arg3 with spaces\" arg4\\ with\\ spaces"
+  )
 
-    XCTAssertEqual(
-      commandLine,
-      CommandLine(
-        command: "./path/to/my command",
-        arguments: [
-          "arg1",
-          "arg2 with spaces",
-          "arg3 with spaces",
-          "arg4 with spaces",
-        ]
-      )
+  #expect(
+    commandLine
+    ==
+    CommandLine(
+      command: "./path/to/my command",
+      arguments: [
+        "arg1",
+        "arg2 with spaces",
+        "arg3 with spaces",
+        "arg4 with spaces",
+      ]
     )
+  )
+}
+
+@Test func testCreationWorkflow() async throws {
+  let directory = FileManager.default.temporaryDirectory
+    .appendingPathComponent("HelloWorld")
+
+  if directory.exists() {
+    try FileManager.default.removeItem(at: directory)
   }
+
+  var creationArguments = ["create", "HelloWorld", "-d", directory.path]
+
+  #if os(macOS)
+    // Without this, the build will fail due to a missing minimum deployment version.
+    creationArguments += ["-t", "SwiftUI"]
+  #endif
+
+  // Ensure creation succeeds
+  await SwiftBundler.main(creationArguments)
+
+  // Ensure fresh project builds
+  await SwiftBundler.main(["bundle", "HelloWorld", "-d", directory.path, "-o", directory.path])
+
+  // Ensure idempotence
+  await SwiftBundler.main(["bundle", "HelloWorld", "-d", directory.path, "-o", directory.path])
 }
