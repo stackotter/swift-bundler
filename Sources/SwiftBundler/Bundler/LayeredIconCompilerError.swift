7ca9320c55fb6d174f812ef58b54b3d6d22e5415
Josh <josh@bashed.sh>
2026-01-27 11:04:59 -0800

    Improve error messages in `LayeredIconCompiler` to use relative paths outside the temp dir

--- Sources/SwiftBundler/Bundler/LayeredIconCompilerError.swift
+++ Sources/SwiftBundler/Bundler/LayeredIconCompilerError.swift
@@ -17,26 +17,27 @@ extension LayeredIconCompiler {
     var userFriendlyMessage: String {
       switch self {
         case .notAnIconFile(let file):
-          return "Expected icon file to have a '.icon' file extension, but got '\(file)'"
+          return
+            "Expected icon file to have a '.icon' file extension, but got '\(file.path(relativeTo: .currentDirectory))'"
         case .failedToCreateIconDirectory(let directory):
           return """
-            Failed to create a temporary icon directory at '\(directory.relativePath)'
+            Failed to create a temporary icon directory at '\(directory)'
             """
         case .failedToCopyFile(let from, let to):
           return """
-            Failed to copy file from '\(from.relativePath)' to '\(to.relativePath)'
+            Failed to copy file from '\(from.path(relativeTo: .currentDirectory))' to '\(to)'
             """
         case .failedToCompileIcon:
           return "Failed to create icon files"
         case .failedToDecodePartialInfoPlist(let plistPath):
           return """
-            Failed to decode the partial Info.plist at '\(plistPath.relativePath)'
+            Failed to decode the partial Info.plist at '\(plistPath)'
             """
         case .failedToCompileICNS:
           return "Failed to convert the icon to ICNS format"
         case .failedToRemoveIconDirectory(let directory):
           return """
-            Failed to remove the temporary icon directory at '\(directory.relativePath)'
+            Failed to remove the temporary icon directory at '\(directory)'
             """
       }
     }
